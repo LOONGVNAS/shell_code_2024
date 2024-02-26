2a979c80694fd1ae946252ac5f9e457c5aded2a9
vans8800 <muwang86@126.com>
2024-02-26 10:57:32 +0800

    Update shift_移动参数.sh

--- shift_移动参数.sh
+++ shift_移动参数.sh
@@ -15,3 +15,21 @@ exit
 
 #Uage
 #shift_usage.sh one two three four
+
+
+$ cat bigshiftparams.sh 
+#!/bin/bash
+# Shifting multiple positions through the parameters 
+#
+echo
+echo "The original parameters: $*"
+echo "Now shifting 2..." 
+shift 2
+echo "Here's the new first parameter: $1" 
+echo
+exit 
+$
+$ ./bigshiftparams.sh alpha bravo charlie delta 
+The original parameters: alpha bravo charlie delta
+Now shifting 2...
+Here's the new first parameter: charlie
