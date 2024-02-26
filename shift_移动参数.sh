88766270bc88a8b4877355710cc97619d84fae78
vans8800 <muwang86@126.com>
2024-02-26 10:52:41 +0800

    Create shift_移动参数.sh

---
+++ shift_移动参数.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Shifting through the parameters 
+#
+echo
+echo "Using the shift method:"
+count=1                                                                              23 
+while [ -n "$1" ]
+do
+  echo "Parameter #$count = $1"
+  count=$[ $count + 1 ] 
+  shift
+done
+echo
+exit 
+
+#Uage
+#shift_usage.sh one two three four
