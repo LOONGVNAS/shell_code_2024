04133e3f18b2d5f485d3570ebfa55c79b6658cad
vans8800 <muwang86@126.com>
2024-02-27 10:07:01 +0800

    Update shell_trap_signals.sh

--- shell_trap_signals.sh
+++ shell_trap_signals.sh
@@ -18,15 +18,42 @@ exit
 $
 
 
+[loongson@localhost ~]$ cat test.sh
+#!/bin/bash
 
-[loongson@localhost ~]$ sh test.sh
-This is a test script.
+#Remove  a set trap
+#
+trap "echo ' Sorry...Ctrl-C is trapped.'" SIGINT
+#
+count=1
+while [ $count -le 3 ]
+do
+     echo "Loop #$count"
+     sleep 1
+     count=$[ $count + 1 ]
+done
+#
+#trap - SIGINT  移除对信号的捕获，使用默认的操作终止脚本运行，按Ctrl-c脚本直接退出 
+# 移除信号捕获后，脚本会按照默认行为处理 SIGINT 信号，也就是终止脚本运行。
+# 忽略对SIGINT 函数的处理，此时按Ctrl-c 终端不会有任何反应
+trap "" SIGINT 
+echo "The trap is now removed."
+#
+count=1
+while [ $count -le 3 ]
+do
+     echo "Loop #$count"
+     sleep 1
+     count=$[ $count + 1 ]
+done
+# 忽略捕获的效果
+[loongson@localhost ~]$ ./test.sh
 Loop #1
 Loop #2
- Sorry! I have trapped Ctrl-C
+ Sorry...Ctrl-C is trapped.
 Loop #3
-Loop #4
- Sorry! I have trapped Ctrl-C
-Loop #5
- Sorry! I have trapped Ctrl-C
-This is the end of test script.
+The trap is now removed.
+Loop #1
+Loop #2
+Loop #3
+
