256c83ebcaa79b00c6b189b1f577c491c42161ca
vans8800 <muwang86@126.com>
2024-02-28 10:54:02 +0800

    Create shell_function_with_255_返回值.sh

---
+++ shell_function_with_255_返回值.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# using the return command in a function 
+function dbl {
+   read -p "Enter a value: " value 
+   echo "doubling the value"
+   return $[ $value * 2 ] 
+}
+dbl
+echo "The new value is $?" 
+
+#当用这种方法从函数中返回值时，一定要小心。为了避免出问题，牢记以下两个技巧。
+#  函数执行一结束就立刻读取返回值。
+#  退出状态码必须介于 0~255。
